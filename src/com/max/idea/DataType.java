9645d2e76e4b8b4dbfad51f6fa60ff07e4c0efbb
arina01100001 <yarochka1234@gmail.com>
2022-03-15 17:28:37 +0500

    second progr

---
+++ src/com/max/idea/DataType.java
@@ -0,0 +1,9 @@
+package com.max.idea;
+
+public class DataType {
+    public static void main(String[] args) {
+        double a = 10.1843082;
+        byte b = 127;
+        System.out.println("double: "+ a + " byte: " + b);
+    }
+}
