5ed52de33c4b9f5f30bce808c552e4fce0e2dbc1
arina01100001 <yarochka1234@gmail.com>
2022-03-15 21:34:31 +0500

    fifth progr

---
+++ src/com/max/idea/Operators2.java
@@ -0,0 +1,34 @@
+package com.max.idea;
+
+import java.util.Scanner;
+
+public class Operators2 {
+    public static void main(String[] args) {
+        System.out.println("Введите 2 целых числа: ");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        System.out.println("Введите арифметическую операцию: ");
+        char operation = input.next().charAt(0);
+
+        int result = 0;
+        switch (operation) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a -b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                result = a/b;
+                break;
+            default:
+                System.out.println("Неизвестная операция");
+       // return result;
+        }
+        System.out.println(result);
+    }
+}
