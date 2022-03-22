1de26f0a509c563912d7ab99e39d151f34aeb997
arina01100001 <yarochka1234@gmail.com>
2022-03-22 20:24:39 +0500

    edit eight progr

--- src/com/max/idea/Cycle2.java
+++ src/com/max/idea/Cycle2.java
@@ -8,15 +8,14 @@ public class Cycle2 {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         int sum = 0;
+        int nechet = 0;
         if (n > 0){
             for (int i = 0; i <=n; i++)
+                if ( i % 2 != 0){
                 sum += i;
         }
-        else System.out.println("Число не положительное");
-
-        if (sum != 0) {
-            System.out.println("Сумма чисел включая введенное число:" + sum);
-        }
+        System.out.println("Сумма чисел включая введенное число:" + sum);
         }
     }
+}
 
